aa5346e0de19eef1cec2dc16cb98e45e2e1b19e9
Alexander Parris <a.b.parris@tcu.edu>
2020-01-13 15:28:45 -0600

    DB part 1

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/blog/<blogType>')
+def blog(blogType):
+    print(blogType)
+    return render_template('blogDisplay.html')
+
+if __name__=='__main__':
+    app.run(debug=True, port=8080)
