297ab544cc0294b1dd20acb9544485718dadce04
Alexander Parris <a.b.parris@tcu.edu>
2020-01-15 08:58:29 -0600

    messing with the database and templates

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-from flask import Flask, render_template
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
+from settings import app, db
 from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
 
 #Make sure to set FLASK_APP=app.py
@@ -21,8 +18,23 @@ def login():
 
 @app.route('/blog/<blogType>')
 def blog(blogType):
-    print(blogType)
-    return render_template('blogDisplay.html')
+    user = models.User(username='Admin', role='admin', password_hash='')
+    db.session.add(user)
+    db.session.commit()
+    user = models.User.query.get(1)
+    print(user)
+    post3 = models.BlogObject(blogType='research', title='post three', image='image', description='Just a short discription', user=user)
+    post4 = models.BlogObject(blogType='research', title='post four', image='image', description='Just a short discription about the post', user=user)
+    post5 = models.BlogObject(blogType='research', title='post five', image='image', description="let's see if this string appears", user=user)
+    post6 = models.BlogObject(blogType='research', title='post six', image='image', description="let's see if this string appears", user=user)
+    post7 = models.BlogObject(blogType='research', title='post seven', image='image', description="let's see if this string appears", user=user)
+    db.session.add(post7)
+    db.session.add(post4)
+    db.session.commit()
+    post8 = models.BlogObject(blogType='research', title='post eight', image='image', description="let's see if this string appears", user=user)
+    posts = models.BlogObject.query.all()
+    print(posts)
+    return render_template('blogDisplay.html', blogType=blogType, posts=posts)
 
 if __name__=='__main__':
     app.run(debug=True, port=8080)
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from app import db
+from settings import db
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,8 +16,9 @@ class User(db.Model):
 class BlogObject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     blogType = db.Column(db.String(10))
+    title = db.Column(db.String(20))
     image = db.Column(db.String(100))
-    timestamp = db.Column(db.DateTime)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     description = db.Column(db.String(240))
     userId = db.Column(db.Integer, db.ForeignKey('user.id'))
 
